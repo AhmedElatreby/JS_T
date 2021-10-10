0c4bf3ad370490108286bf71196ea61abc4b7d1f
ahmedsport <ahmedsport@hotmail.com>
2021-10-10 16:29:51 +0100

    JS

--- main.js
+++ main.js
@@ -276,83 +276,83 @@
 // // ELEMENT SELECTORS
 
 // Single Element Selectors
-console.log(document.getElementById("my-form"));
-console.log(document.querySelector(".container"));
+// console.log(document.getElementById("my-form"));
+// console.log(document.querySelector(".container"));
 // Multiple Element Selectors
-console.log(document.querySelectorAll(".item"));
-console.log(document.getElementsByTagName("li"));
-console.log(document.getElementsByClassName("item"));
+// console.log(document.querySelectorAll(".item"));
+// console.log(document.getElementsByTagName("li"));
+// console.log(document.getElementsByClassName("item"));
 
-const items = document.querySelectorAll(".item");
-items.forEach((item) => console.log(item));
+// const items = document.querySelectorAll(".item");
+// items.forEach((item) => console.log(item));
 
 // MANIPULATING THE DOM
-const ul = document.querySelector(".items");
+// const ul = document.querySelector(".items");
 // ul.remove();
 // ul.lastElementChild.remove();
-ul.firstElementChild.textContent = "Hello";
-ul.children[1].innerText = "Brad";
-ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
+// ul.firstElementChild.textContent = "Hello";
+// ul.children[1].innerText = "Brad";
+// ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
 
-const btn = document.querySelector(".btn");
+// const btn = document.querySelector(".btn");
 // btn.style.background = 'red';
 
 // EVENTS
 
 // Mouse Event
-btn.addEventListener("click", (e) => {
-	e.preventDefault();
-	console.log(e.target.className);
-	document.getElementById("my-form").style.background = "#ccc";
-	document.querySelector("body").classList.add("bg-dark");
-	ul.lastElementChild.innerHTML = "<h1>Changed</h1>";
-});
+// btn.addEventListener("click", (e) => {
+// 	e.preventDefault();
+// 	console.log(e.target.className);
+// 	document.getElementById("my-form").style.background = "#ccc";
+// 	document.querySelector("body").classList.add("bg-dark");
+// 	ul.lastElementChild.innerHTML = "<h1>Changed</h1>";
+// });
 
 // Keyboard Event
-const nameInput = document.querySelector("#name");
-nameInput.addEventListener("input", (e) => {
-	document.querySelector(".container").append(nameInput.value);
-});
+// const nameInput = document.querySelector("#name");
+// nameInput.addEventListener("input", (e) => {
+// 	document.querySelector(".container").append(nameInput.value);
+// });
 
 // USER FORM SCRIPT
 
-// // Put DOM elements into variables
-// const myForm = document.querySelector("#my-form");
-// const nameInput = document.querySelector("#name");
-// const emailInput = document.querySelector("#email");
-// const msg = document.querySelector(".msg");
-// const userList = document.querySelector("#users");
+// Put DOM elements into variables
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
 
 // // Listen for form submit
-// myForm.addEventListener("submit", onSubmit);
+myForm.addEventListener("submit", onSubmit);
 
-// function onSubmit(e) {
-// 	e.preventDefault();
+function onSubmit(e) {
+	e.preventDefault();
 
-// 	if (nameInput.value === "" || emailInput.value === "") {
-// 		// alert('Please enter all fields');
-// 		msg.classList.add("error");
-// 		msg.innerHTML = "Please enter all fields";
+	if (nameInput.value === "" || emailInput.value === "") {
+		// alert("Please enter all fields");
+		msg.classList.add("error");
+		msg.innerHTML = "Please enter all fields";
 
-// 		// Remove error after 3 seconds
-// 		setTimeout(() => msg.remove(), 3000);
-// 	} else {
-// 		// Create new list item with user
-// 		const li = document.createElement("li");
+		// Remove error after 3 seconds
+		setTimeout(() => msg.remove(), 3000);
+	} else {
+		// Create new list item with user
+		const li = document.createElement("li");
 
-// 		// Add text node with input values
-// 		li.appendChild(
-// 			document.createTextNode(`${nameInput.value}: ${emailInput.value}`)
-// 		);
+		// Add text node with input values
+		li.appendChild(
+			document.createTextNode(`${nameInput.value}: ${emailInput.value}`)
+		);
 
-// 		// Add HTML
-// 		// li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+		// Add HTML
+		li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
 
-// 		// Append to ul
-// 		userList.appendChild(li);
+		// Append to ul
+		userList.appendChild(li);
 
-// 		// Clear fields
-// 		nameInput.value = "";
-// 		emailInput.value = "";
-// 	}
-// }
+		// Clear fields
+		nameInput.value = "";
+		emailInput.value = "";
+	}
+}
